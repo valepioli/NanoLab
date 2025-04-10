86ae9a1246a5f681af33f8df0ee3a708033ba53e
sollau <lau.solieri@gmail.com>
2025-04-10 15:31:32 +0200

    write instructions for graphs in data_analysis.py

--- DC/data_analysis.py
+++ DC/data_analysis.py
@@ -1,1 +1,20 @@
-#ciao Laura
+""" 
+PLOT INSTRUCTIONS
+
+# plot 1: Transfer Characteristic I_D vs V_G (linear linear, log linear) 
+        -> find mobility
+        -> find V_t
+        -> find S
+        -> find V_on
+        -> find I_on/I_off 
+
+# plot 2: Output characteristics I_D vs V_D 
+        -> see general behaviour
+
+# plot 3: [use the semilog from plot 1]
+        -> find S
+
+# plot 4: Sqrt(I_D) vs V_G
+        -> alternative for finding V_t
+
+"""
